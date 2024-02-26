4f8ecadfbc5c9220ce8cfbafa11c6a06a864dc5f
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2024-02-26 18:45:43 +0200

    Update rpc sample

--- rpc/client.py
+++ rpc/client.py
@@ -1,29 +1,53 @@
-from uuid import uuid4
+#!/usr/bin/env python
+import uuid
 
 import pika
 
 
-def on_message(ch, method, properties, body):
-    print(f"REPLY_FROM_SERVER: {body}")
+class FibonacciRpcClient(object):
+
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host="localhost")
+        )
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue="", exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True,
+        )
+
+        self.response = None
+        self.corr_id = None
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange="",
+            routing_key="rpc_queue",
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=str(n),
+        )
+        while self.response is None:
+            self.connection.process_data_events(time_limit=None)
+        return int(self.response)
 
 
-connection_parameters = pika.ConnectionParameters(host="localhost")
-connection = pika.BlockingConnection(connection_parameters)
+fibonacci_rpc = FibonacciRpcClient()
 
-channel = connection.channel()
-reply_queue = channel.queue_declare(queue="")
-channel.basic_consume(
-    queue=reply_queue.method.queue, auto_ack=True, on_message_callback=on_message
-)
-
-channel.queue_declare(queue="requests")
-channel.basic_publish(
-    exchange="",
-    routing_key="requests",
-    body="REQUESTING_REPLY",
-    properties=pika.BasicProperties(
-        reply_to=reply_queue.method.queue, correlation_id=str(uuid4())
-    ),
-)
-
-channel.start_consuming()
+print(" [x] Requesting fib(30)")
+response = fibonacci_rpc.call(30)
+print(f" [.] Got {response}")
